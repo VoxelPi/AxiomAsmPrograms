45c252adc36fd1c5fced2edb1a66a1c9cff600e0
Peter Smek <smek.peter@gmail.com>
2025-05-31 15:30:02 +0200

    Always use newest axiom cli jar

--- axasm.bat
+++ axasm.bat
@@ -13,10 +13,11 @@ if not exist "%AXIOM_TOOLS_PATH%\" (
     exit /b 1
 )
 
-REM Find the JAR file with the pattern axiom-cli-*.jar in the specified folder
+REM Find all JAR files with the pattern axiom-cli-*.jar in the specified folder and sort them alphabetically
 set "JARFILE="
-for %%f in ("%AXIOM_TOOLS_PATH%\axiom-cli-*.jar") do (
+for /f "delims=" %%f in ('dir /b /o-n "%AXIOM_TOOLS_PATH%\axiom-cli-*.jar"') do (
     set "JARFILE=%%f"
+    REM Only keep the first file (which will be the highest version due to reverse alphabetical order)
     goto :found
 )
 
@@ -24,4 +25,4 @@ echo ERROR: No JAR file matching "axiom-cli-*.jar" found in "%AXIOM_TOOLS_PATH%"
 exit /b 1
 
 :found
-java -jar "!JARFILE!" assemble %*
+java -jar "%AXIOM_TOOLS_PATH%\!JARFILE!" assemble %*
